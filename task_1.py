8ad4f604c12ca7741eaa68fffc46610c9310dd01
dat13dmy <dat13dmy@student.lu.se>
2019-04-14 16:54:45 +0200

    task_1halfway

--- task_1.py
+++ task_1.py
@@ -1,69 +1,60 @@
 import numpy as np
+from scipy.fftpack import fft, fftshift
 import matplotlib.pyplot as plt
-qpsk_constellation = np.array([complex(1, 1), complex(-1, 1), complex(-1, -1), complex(1, -1)])
 
+uk = np.random.randint(2, size=10)
+jj = 0
+ck = []
+a = complex()
+for e in range(0, 5):
+    if uk[jj] == 0 and uk[jj+1] == 0:
+            a = complex(1, 1)
+    elif uk[jj] == 1 and uk[jj+1] == 0:
+            a = complex(-1, 1)
+    elif uk[jj] == 0 and uk[jj+1] == 1:
+            a = complex(1, -1)
+    elif uk[jj] == 1 and uk[jj+1] == 1:
+            a = complex(-1, -1)
+    if jj == 0:
+        ck.append(a)
+    else:
+        k = jj/2
+        ck.append(a)
+    jj+=2
 
-def qpsk_modulation(bits):
-    """
+    T = 1  # Symbol duration
 
-    :param bits: Integer representation of the bits to be modulated. For BPSK Integer and Binary are the same.
-    :type bits: int
-    :return: The complex constellation symbol
-    :rtype: complex
-    """
-    try:
-        return qpsk_constellation[bits]
-    except IndexError:
-        raise ValueError('{} is to large for this constellation.'.format(bin(bits)))
+    t = np.linspace(-15.0, 15.0, int(1.0e4))  # Time interval
+    g = [(1 if (0 <= t_i <= T) else 0) for t_i in t]  # Rectangular pulse, length T
 
+    f = fftshift(np.fft.fftfreq(len(t), t[1] - t[0]))
+    G_f = fftshift(fft(g)) * (T / len(t))
 
-def qpsk_demodulation(received_symbol):
-    """
+    f_theo = np.linspace(-4, 4, int(1.0e3))
+    x = f_theo * T
+    G_f_theo = T * np.sinc(x) * np.exp(-1j * x)
 
-    :param received_symbol: The complex symbol to be demodulated.
-    :type received_symbol: complex
-    :return: The demodulated bits, represented as the corresponding Integer value.
-    :rtype: int
-    """
-    if received_symbol.real < 0 and received_symbol.imag > 0:
-        return complex(1, 1)
-    elif received_symbol.real < 0 and received_symbol.imag < 0:
-        return complex(-1, 1)
-    elif received_symbol.real > 0 and received_symbol.imag < 0:
-        return complex(-1, -1)
-    elif received_symbol.real > 0 and received_symbol.imag > 0:
-        return complex(1, -1)
+    plt.rcParams['figure.figsize'] = [12, 6]
 
-
-def plot_constellation(constellation, base=4):
-    # Extract the x and y values for plotting
-    in_phase = [symbol.real for symbol in constellation]
-    quadrature = [symbol.imag for symbol in constellation]
-
-    # The code word size, for BPSK: code_word_size k = 1
-    # Constellation size: also called alphabet M = 2^k,
-    # where k is the number of bits in each symbol (code word size).
-    code_word_size = int(np.log2(np.size(constellation)))
-
-    # Plot the constellatoin points
     plt.figure()
-    plt.plot(in_phase, quadrature, 'o')
-    plt.title("Constellation diagram")
-    plt.xlabel("In-phase")
-    plt.ylabel("Quadrature")
+
+    # Plot the basis function
+    plt.subplot(1, 2, 1)
+    plt.plot(t / T, g)
+    plt.xlim(-4, 4)
     plt.grid(True)
+    plt.xlabel(r'$t/T$', fontsize=14)
+    plt.ylabel(r'$g_R(t)$', fontsize=14)
 
-    # The following snippet of code just adds strings to the plot
-    # with the bits the symbol is representing
-    count = 0
-    for symbol in constellation:
-        if base == 2:
-            plt_str = '{0:0{1:d}b}'.format(count, int(code_word_size))
-        if base == 10:
-            plt_str = '{0:d}'.format(count)
-        # x and y cordinates for the string are the real and imaginary parts of the symbol.
-        plt.text(symbol.real, symbol.imag, plt_str)
-        count += 1
+    # Plot the spectrum of the pulse
+    plt.subplot(1, 2, 2)
+    plt.plot(f * T, np.abs(G_f) / np.max(np.abs(G_f)), label=r'$FFT\{g_R(t, T)\}$')
+    plt.plot(f_theo * T, np.abs(G_f_theo) / T, '--', label=r'$\mathcal{F} \{g_R(t, T)\}$')
+    plt.xlim(-4, 4)
+    plt.grid(True)
+    plt.xlabel(r'$f\cdot T$', fontsize=14)
+    plt.ylabel(r'$|G_R(f)| / T$', fontsize=14)
+    plt.legend(prop={'size': 14})
 
-    # Finally show the plot
     plt.show()
+
